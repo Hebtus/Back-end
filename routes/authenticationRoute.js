992aa3fcddc2a2823d1b63b2c5938d80393323d2
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-03-17 04:04:50 +0200

    updated authentication with protect and restrict

--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -6,16 +6,20 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get('/logout', authController.logout);
+router.post('/forgotpassword', authController.forgotPassword);
 router.get(
   '/signup-confirm/:emailConfirmationToken',
   authController.confirmEmail
 );
+//from here down add requests that are available after u r logged in only
+//remeber to add the berarer token to the autherization in postman
+router.use('/protect', authController.protect);
 
-router.post('/forgotpassword', authController.forgotPassword);
-
+router.get('/logout', authController.logout);
 router.get('/facebook', authController.facebookLogin);
 router.get('/google', authController.googleLogin);
 router.patch('/resetPassword/:token', authController.resetPassword);
 router.patch('/updatepassword/:token', authController.updatePassword);
+//from here down add whatever requests that are avialble to creators only
+router.use('/restrict', authController.restrictTo('admin'));
 module.exports = router;

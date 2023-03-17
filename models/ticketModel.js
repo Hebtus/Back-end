c4fa101eb1e8f5c149c1620f00bd57a7ecd18972
Joseph Ameer Aziz <joseph.ameer21@gmail.com>
2023-03-17 03:58:38 +0200

    Added Some validations to TicketsModel

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -5,23 +5,28 @@ const ticketSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide a ticket name'],
+    minlength: [1, 'Event Name can not be less than 1 character.'],
+    maxlength: [30, 'Event Name can not be more than 30 characters long.'],
   },
   type: {
     type: String,
     required: [true, 'Please specify the ticket type'],
     enum: {
       //ticket types on the site itself are not in vip or not no
-      values: ['Paid', 'Free', 'Donation'],
+      values: ['Regular', 'VIP'],
       message: '{VALUE} is not supported',
     },
   },
   price: {
     type: Number,
-    required: [true, 'A ticketr must have a price'],
+    required: [true, 'A ticket must have a price'],
+    min: 0,
   },
   capacity: {
     type: Number,
     required: [true, 'please sepcify ticket capacity'],
+    max: [10000000, 'Maximum Conceivable capacity reached'],
+    default: 1,
   },
   sellingStartTime: {
     type: Date,
@@ -34,6 +39,8 @@ const ticketSchema = new mongoose.Schema({
   },
   currentReservations: {
     type: Number,
+    default: 0,
+    max: [10000000, 'Maximum Conceivable capacity reached'],
   },
 });
 

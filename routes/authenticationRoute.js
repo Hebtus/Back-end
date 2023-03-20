8ea2310e2334e2c5f174a5e7d7589f1852074715
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-03-20 20:34:40 +0200

    facebook and google route

--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -8,16 +8,17 @@ router.post('/signup', authController.signup);
 router.get('/signup-confirm/:confirmationToken', authController.confirmEmail);
 router.post('/login', authController.login);
 router.post('/forgotpassword', authController.forgotPassword);
-//from here down add requests that are available after u r logged in only
-//remeber to add the berarer token to the autherization in postman
-router.use(authController.protect);
-
-router.get('/logout', authController.logout);
 router.get('/login/facebook', authController.facebookLogin);
 router.get(
   '/login/google',
   passport.authenticate('google', { scope: ['profile', 'email'] })
 );
+//from here down add requests that are available after u r logged in only
+//remeber to add the berarer token to the autherization in postman
+router.use(authController.protect);
+
+router.get('/logout', authController.logout);
+
 router.patch('/deactivate', authController.deactivateAccount);
 router.patch('/resetpassword/:token', authController.resetPassword);
 router.patch('/updatepassword', authController.updatePassword);

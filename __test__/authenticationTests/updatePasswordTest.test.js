746e98d854b8571b20d4911b6733a40bc9e210e1
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-03-22 20:07:44 +0200

    updated testing

--- __test__/authenticationTests/updatePasswordTest.test.js
+++ __test__/authenticationTests/updatePasswordTest.test.js
@@ -1,9 +1,11 @@
 const request = require('supertest');
+// const session = require('express-session');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const { doesNotMatch } = require('assert');
 const User = require('../../models/userModel');
 const app = require('../../app');
+// const app = require('../../utils/config/config.env');
 
 dotenv.config({ path: './config.env' });
 
@@ -20,7 +22,6 @@ beforeAll(async () => {
     .then(() => {
       console.log('TestDB is connected successfuly!');
     });
-
   await mongoose.connection.collection('users').deleteMany({});
   const user = await User.create({
     name: {
@@ -30,46 +31,52 @@ beforeAll(async () => {
     email: 'user70@gmail.com',
     password: '12345678',
   });
+});
 
-  test('Check User input inCorrect password', async () => {
-    const res = await request(app)
-      .post('/api/v1/updatepassword')
-      .send({
-        passwordCurrent: 'kambotchas',
-        password: 'kambotchase',
-        confirmPassword: 'kambotchase',
-      })
-      .expect(401);
-    expect(res.body.message).toMatch('Your current password is wrong.');
-    // console.log(res);
-    // done();
-  });
+test('Check User input inCorrect password', async () => {
+  const res = await request(app)
+    .post('/api/v1/updatepassword')
+    .send({
+      passwordCurrent: 'kambotchas',
+      password: 'kambotchase',
+      confirmPassword: 'kambotchase',
+    })
+    .expect(401);
+  expect(res.body.message).toMatch('Your current password is wrong.');
+  // console.log(res);
+  // done();
+});
 
-  test('Check password confirmation mismatch', async () => {
-    const res = await request(app)
-      .post('/api/v1/updatepassword')
-      .send({
-        passwordCurrent: '12345678',
-        password: 'kambotchas',
-        confirmPassword: 'kambotchases',
-      })
-      .expect(401);
-    expect(res.body.message).toMatch('confirm password doesnt match');
-    // console.log(res);
-    // done();
-  });
+test('Check password confirmation mismatch', async () => {
+  const res = await request(app)
+    .post('/api/v1/updatepassword')
+    .send({
+      passwordCurrent: '12345678',
+      password: 'kambotchas',
+      confirmPassword: 'kambotchases',
+    })
+    .expect(401);
+  expect(res.body.message).toMatch('confirm password doesnt match');
+  // console.log(res);
+  // done();
+});
 
-  test('Check password and update password correct', async () => {
-    const res = await request(app)
-      .post('/api/v1/updatepassword')
-      .send({
-        passwordCurrent: '12345678',
-        password: 'kambotchas',
-        confirmPassword: 'kambotchas',
-      })
-      .expect(200);
-    expect(res.body.message).toMatch('password updated successfully');
-    // console.log(res);
-    // done();
-  });
+test('Check password and update password correct', async () => {
+  const res = await request(app)
+    .post('/api/v1/updatepassword')
+    .send({
+      passwordCurrent: '12345678',
+      password: 'kambotchas',
+      confirmPassword: 'kambotchas',
+    })
+    .expect(200);
+  expect(res.body.message).toMatch('password updated successfully');
+  // console.log(res);
+  // done();
+});
+
+afterAll(async () => {
+  // await mongoose.connection.collection('users').deleteMany({});
+  await User.deleteMany();
+  mongoose.disconnect();
 });

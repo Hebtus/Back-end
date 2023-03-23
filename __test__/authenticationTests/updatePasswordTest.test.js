a4cf5b45252358ab8c585875edecf25aeddb2f94
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-03-23 03:57:30 +0200

    updated updatesPass test

--- __test__/authenticationTests/updatePasswordTest.test.js
+++ __test__/authenticationTests/updatePasswordTest.test.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose');
 const { doesNotMatch } = require('assert');
 const User = require('../../models/userModel');
 const app = require('../../app');
-// const app = require('../../utils/config/config.env');
+const createConfirmedUser = require('../testutils/createConfirmedUser');
+const loginConfirmedUser = require('../testutils/loginConfirmedUser');
 
+// dotenv.config({ path: './utils/config/config.env' });
 dotenv.config({ path: './config.env' });
 
 const DBstring = process.env.TEST_DATABASE;
@@ -23,27 +25,13 @@ beforeAll(async () => {
       console.log('TestDB is connected successfuly!');
     });
   await mongoose.connection.collection('users').deleteMany({});
-  const user = await User.create({
-    name: {
-      firstName: 'yasmina',
-      lastName: 'hashem',
-    },
-    email: 'user70@gmail.com',
-    password: '12345678',
-    accountConfirmation: 1,
-  });
 });
-
 test('Check User input inCorrect password', async () => {
-  const loginres = await request(app).post('/api/v1/updatepassword').send({
-    passwordCurrent: 'kambotchas',
-    password: 'kambotchase',
-    confirmPassword: 'kambotchase',
-  });
-  // .expect(401);
-
+  await createConfirmedUser.createTestUser();
+  const jwtToken = await loginConfirmedUser.loginUser();
   const res = await request(app)
     .post('/api/v1/updatepassword')
+    .set('authorization', `Bearer ${jwtToken}`)
     .send({
       passwordCurrent: 'kambotchas',
       password: 'kambotchase',
@@ -56,10 +44,13 @@ test('Check User input inCorrect password', async () => {
 });
 
 test('Check password confirmation mismatch', async () => {
+  await createConfirmedUser.createTestUser();
+  const jwtToken = await loginConfirmedUser.loginUser();
   const res = await request(app)
     .post('/api/v1/updatepassword')
+    .set('authorization', `Bearer ${jwtToken}`)
     .send({
-      passwordCurrent: '12345678',
+      passwordCurrent: '123456789',
       password: 'kambotchas',
       confirmPassword: 'kambotchases',
     })
@@ -69,11 +60,14 @@ test('Check password confirmation mismatch', async () => {
   // done();
 });
 
-test('Check password and update password correct', async () => {
+test('Check password confirmation mismatch', async () => {
+  await createConfirmedUser.createTestUser();
+  const jwtToken = await loginConfirmedUser.loginUser();
   const res = await request(app)
     .post('/api/v1/updatepassword')
+    .set('authorization', `Bearer ${jwtToken}`)
     .send({
-      passwordCurrent: '12345678',
+      passwordCurrent: '123456789',
       password: 'kambotchas',
       confirmPassword: 'kambotchas',
     })
@@ -82,9 +76,8 @@ test('Check password and update password correct', async () => {
   // console.log(res);
   // done();
 });
-
 afterAll(async () => {
   // await mongoose.connection.collection('users').deleteMany({});
-  await User.deleteMany();
+  // await User.deleteMany();
   mongoose.disconnect();
 });

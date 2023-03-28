3e19724e4c932c5db10b16b8c99dc20df2297aa0
MohamedHussein-X <76919713+MohamedHussein-X@users.noreply.github.com>
2023-03-28 23:01:51 +0200

    Update server.js
    I have commented tests :)

--- server.js
+++ server.js
@@ -19,59 +19,58 @@ const DBstring =
     ? process.env.DATABASE_LOCAL
     : process.env.DATABASE;
 
-mongoose
-  .connect(DBstring, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(async () => {
-    console.log('DB is connected successfuly!');
+mongoose.connect(DBstring, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+// .then(async () => {
+//   console.log('DB is connected successfuly!');
 
-    //for testing and saving email creditsssssss
-    //if no user create confirmed user
-    const anyuser = await User.find();
-    // console.log(anyuser);
-    // if (anyuser.length === 0) test.createTestUser();
-    // if (anyuser.length === 0) {
-    //   Seeder.Seed();
-    // }
-    await mongoose.connection.db.dropDatabase();
+//   //for testing and saving email creditsssssss
+//   //if no user create confirmed user
+//   const anyuser = await User.find();
+//   // console.log(anyuser);
+//   // if (anyuser.length === 0) test.createTestUser();
+//   // if (anyuser.length === 0) {
+//   //   Seeder.Seed();
+//   // }
+//   await mongoose.connection.db.dropDatabase();
 
-    // console.log('DB is removed successfuly!');
-    //Schema Testing
-    const testEvent = new Event({
-      name: 'loleventxd',
-      startDate: Date.now(),
-      endtDate: Date.now() + 1000 * 60 * 60 * 24 * 10, //after 10 days
-      privacy: false,
-    });
+//   // console.log('DB is removed successfuly!');
+//   //Schema Testing
+//   const testEvent = new Event({
+//     name: 'loleventxd',
+//     startDate: Date.now(),
+//     endDate: Date.now() + 1000 * 60 * 60 * 24 * 10, //after 10 days
+//     privacy: false,
+//   });
 
-    const testTickets = new Tickets({
-      name: '2a3det 4ayTicket',
-      type: 'Regular',
-      price: 100,
-      capacity: 10,
-      sellingStartTime: Date.now() + 1000 * 60 * 60 * 24 * 1,
-      sellingEndTime: Date.now() + 1000 * 60 * 60 * 24 * 2,
-    });
+//   const testTickets = new Tickets({
+//     name: '2a3det 4ayTicket',
+//     type: 'Regular',
+//     price: 100,
+//     capacity: 10,
+//     sellingStartTime: Date.now() + 1000 * 60 * 60 * 24 * 1,
+//     sellingEndTime: Date.now() + 1000 * 60 * 60 * 24 * 2,
+//   });
 
-    const testBooking = new Booking({
-      name: { firstName: 'lol', lastName: 'attendeelastname' },
-      gender: 'Male',
-      phoneNumber: 12345678910,
-      guestEmail: 'irushbullet@google.com',
-      price: 100,
-      quantity: 2,
-      purchasedOn: Date.now() + 1000 * 60 * 60 * 24 * 1,
-    });
-    await testEvent.save().then(async () => {
-      testTickets.eventID = testEvent._id;
-      await testTickets.save().then(() => {
-        testBooking.ticketID = testTickets._id;
-        testBooking.save();
-      });
-    });
-  });
+//   const testBooking = new Booking({
+//     name: { firstName: 'lol', lastName: 'attendeelastname' },
+//     gender: 'Male',
+//     phoneNumber: 12345678910,
+//     guestEmail: 'irushbullet@google.com',
+//     price: 100,
+//     quantity: 2,
+//     purchasedOn: Date.now() + 1000 * 60 * 60 * 24 * 1,
+//   });
+//   await testEvent.save().then(async () => {
+//     testTickets.eventID = testEvent._id;
+//     await testTickets.save().then(() => {
+//       testBooking.ticketID = testTickets._id;
+//       testBooking.save();
+//     });
+//   });
+// });
 
 // const testerfunc = async () => {
 //   const testUser = new User({

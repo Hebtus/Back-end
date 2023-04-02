2d92387707887832a9e57142589cbac874ddbf1c
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-04-02 22:31:21 +0200

    added protect to creator route

--- models/ticketModel.js
+++ models/ticketModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const Event = require('./eventModel');
+
 const ticketSchema = new mongoose.Schema({
   name: {
     type: String,
--- routes/creatorRoute.js
+++ routes/creatorRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 // const authController = require('../controllers/authenticationController');
 const creatorController = require('../controllers/creatorController');
+const authController = require('../controllers/authenticationController');
 
 const router = express.Router();
 
 // router.route('/').get(creatorController.getAllEvents);
 
-router.route('/:id').get(creatorController.getEvent);
+router.route('/:id').get(authController.protect, creatorController.getEvent);
 
 // router.route('/events/:id/sales').get(creatorController.getSales);
 

c93278ad82f204eac90235e9dee34cf596e98fcc
MohamedHussein-X <76919713+MohamedHussein-X@users.noreply.github.com>
2023-04-10 17:19:08 +0200

    Add booking main route to app.js

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,7 +1,8 @@
-// const express = require('express');
-// const authController = require('../controllers/authenticationController');
-// const bookingController = require('../controllers/bookingController');
+const express = require('express');
+const authController = require('../controllers/authenticationController');
+const bookingController = require('../controllers/bookingController');
 
+const router = express.Router();
 // //allows nested routing
 // //and lets the controller get access to previous parameters
 // const router = express.Router({ mergeParams: true });
@@ -13,4 +14,7 @@
 
 // router.route('/:id').get(bookingController.getBooking);
 
-// //add attendee?
+// //add attendee
+router.post('/add-attendee/', bookingController.addAttendee);
+
+module.exports = router;

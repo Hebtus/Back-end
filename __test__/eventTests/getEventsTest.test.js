2f2870151d777d7882a3d97fbffbe8c5011b73f0
Joseph Ameer Aziz <joseph.ameer21@gmail.com>
2023-04-15 18:22:12 +0200

    Get Event Tests

--- __test__/eventTests/getEventsTest.test.js
+++ __test__/eventTests/getEventsTest.test.js
@@ -39,43 +39,42 @@ beforeAll(async () => {
   });
   userID = user._id;
 });
-// jest.setTimeout(60000);
-// test('Check GeoQuery Works Correctly: Events Within range  ', async () => {
-//   const testEvents = testEventSeeder.eventsWithinUserRange([userID]);
-//   await Event.create(testEvents);
-//   const res = await request(app).get('/api/v1/events').send().expect(200);
-//   expect(res.body.data.events.length).toEqual(20);
+test('Check GeoQuery Works Correctly: Events Within range  ', async () => {
+  const testEvents = testEventSeeder.eventsWithinUserRange([userID]);
+  await Event.create(testEvents);
+  const res = await request(app).get('/api/v1/events').send().expect(200);
+  expect(res.body.data.events.length).toEqual(20);
 
-//   await Event.deleteMany();
-//   await User.deleteMany();
-// });
+  await Event.deleteMany();
+  await User.deleteMany();
+});
 
-// test('Check GeoQuery Works Correctly: Events Out of range  ', async () => {
-//   const testEvents = testEventSeeder.eventsNotWithinRange([userID]);
-//   await Event.create(testEvents);
-//   const res = await request(app).get('/api/v1/events').send().expect(200);
-//   expect(res.body.data.events.length).toEqual(0);
-//   console.log('res', res.body);
+test('Check GeoQuery Works Correctly: Events Out of range  ', async () => {
+  const testEvents = testEventSeeder.eventsNotWithinRange([userID]);
+  await Event.create(testEvents);
+  const res = await request(app).get('/api/v1/events').send().expect(200);
+  expect(res.body.data.events.length).toEqual(0);
+  console.log('res', res.body);
 
-//   await User.deleteMany();
-//   await Event.deleteMany();
-// });
+  await User.deleteMany();
+  await Event.deleteMany();
+});
 
-// test('Check StartDate Works Correctly: Events Not Within  Date  ', async () => {
-//   const testEvents = testEventSeeder.eventsNotWithinRange([userID]);
-//   await Event.create(testEvents);
-//   const res = await request(app)
-//     .get(
-//       '/api/v1/events?startDate=2023-05-10T10:16:10.467z&endDate=2023-05-11T15:16:10.467z'
-//     )
-//     .send()
-//     .expect(200);
-//   expect(res.body.data.events.length).toEqual(0);
-//   console.log('res', res.body);
+test('Check StartDate Works Correctly: Events Not Within  Date  ', async () => {
+  const testEvents = testEventSeeder.eventsNotWithinRange([userID]);
+  await Event.create(testEvents);
+  const res = await request(app)
+    .get(
+      '/api/v1/events?startDate=2023-05-10T10:16:10.467z&endDate=2023-05-11T15:16:10.467z'
+    )
+    .send()
+    .expect(200);
+  expect(res.body.data.events.length).toEqual(0);
+  console.log('res', res.body);
 
-//   await User.deleteMany();
-//   await Event.deleteMany();
-// });
+  await User.deleteMany();
+  await Event.deleteMany();
+});
 
 test('Check StartDate Works Correctly: Events Within  Date  ', async () => {
   const startDate = new Date('2023-05-2');
@@ -91,15 +90,12 @@ test('Check StartDate Works Correctly: Events Within  Date  ', async () => {
   await Event.create(testEvents);
   const res = await request(app)
     .get(
-      '/api/v1/events?startDate=2023-05-10T10:16:10.467z&endDate=2023-05-11T15:16:10.467z'
+      '/api/v1/events/?startDate=2023-05-09T10:16z&endDate=2023-05-11T15:00z'
     )
     .send()
     .expect(200);
-  expect(res.body.data.events.length).toEqual(20);
+  expect(res.body.data.events.length).toEqual(10);
   console.log('res', res.body);
-
-  await User.deleteMany();
-  await Event.deleteMany();
 });
 
 afterAll(async () => {

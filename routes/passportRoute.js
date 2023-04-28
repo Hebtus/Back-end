0c5749136df9cb0d3c523a6b24e3a77323650642
Joseph Ameer Aziz <joseph.ameer21@gmail.com>
2023-04-28 15:15:10 +0300

    Another Google Authentication for Cross-Platform

--- routes/passportRoute.js
+++ routes/passportRoute.js
@@ -7,6 +7,12 @@ const express = require('express');
 const passport = require('passport');
 const goolgePassportAuth = require('../passport/googlepassportAuth');
 const facebookPassportAuth = require('../passport/facebookpassportAuth');
+const { OAuth2Client } = require('google-auth-library');
+//check that this works
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const User = require('../models/userModel');
+const authenticationController = require('../controllers/authenticationController');
+const catchAsync = require('../utils/catchAsync');
 
 dotenv.config({ path: './config.env' });
 goolgePassportAuth.googleAuth(passport);
@@ -63,5 +69,33 @@ router.get(
     res.redirect(`${process.env.FRONTEND_URL}/login`);
   }
 );
+
+const AuthenticateGoogle = catchAsync(async (req, res, next) => {
+  if (!req.body.tokenId)
+    return res.status(400).send('You must provide Google tokenId');
+  const token = req.body.tokenId;
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGEL_CLIENT_ID,
+  });
+  const { name, email } = ticket.getPayload();
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    user = new User({
+      name: {
+        firstName: name.split(' ')[0],
+        lastName: name.split(' ')[1],
+      },
+      email: email,
+      GoogleID: tokenId,
+      password: Math.random().toString().substr(2, 10),
+      accountConfirmation: 1,
+    });
+    await user.save();
+  }
+  authenticationController.createSendToken(user, 201, res);
+});
+
+router.post('/login/google', AuthenticateGoogle);
 
 module.exports = router;
